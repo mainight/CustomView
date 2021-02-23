389a13d030a6a4d3456c9abad8a039a14a872c63
mainight <39176507+mainight@users.noreply.github.com>
2021-02-23 23:07:21 +0800

    组合控件

--- app/src/main/java/com/kotlin/customview/MainActivity.java
+++ app/src/main/java/com/kotlin/customview/MainActivity.java
@@ -3,12 +3,26 @@ package com.kotlin.customview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.kotlin.customview.view.CustomTitleView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CustomTitleView mCustomTitleView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       //自定义view，通过组合控件的方式实现
+        setContentView(R.layout.activity_custom_view_compose_demo);
+        mCustomTitleView=findViewById(R.id.customview_title);
+        mCustomTitleView.setTitle("This is Title");
+        mCustomTitleView.setmLeftOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/kotlin/customview/view/CustomTitleView.java
@@ -0,0 +1,44 @@
+package com.kotlin.customview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.kotlin.customview.R;
+
+public class CustomTitleView extends FrameLayout implements View.OnClickListener {
+    private View.OnClickListener mLeftOnClickListener;
+    private Button mBackBtn;
+    private TextView mTitleView;
+    public CustomTitleView(Context context,AttributeSet attrs){
+        super(context,attrs);
+        LayoutInflater.from(context).inflate(R.layout.custom_title_view,this);
+        mBackBtn=findViewById(R.id.btn_left);
+        mBackBtn.setOnClickListener(this);
+        mTitleView=findViewById(R.id.title_tv);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_left:
+                if (mLeftOnClickListener!=null){
+                    mLeftOnClickListener.onClick(v);
+                }
+                break;
+        }
+    }
+
+    public void setmLeftOnClickListener(OnClickListener mLeftOnClickListener) {
+        this.mLeftOnClickListener = mLeftOnClickListener;
+    }
+
+    public void setTitle(String title){
+        mTitleView.setText(title);
+    }
+}
